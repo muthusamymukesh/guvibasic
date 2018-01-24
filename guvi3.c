392e0bcd2cb78fb4c5f20535565e0ade7a37f8e3
shivam2060 <shivam.agrawal_cs15@gla.ac.in>
2018-01-24 22:04:03 +0530

    vowel

---
+++ guvi3.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+void main()
+{
+	char i;
+	scanf("%c",&i);
+	if(i='a'||i='e'||i='i'||i='o'||i='u'||i='A'||i='E'||i='I'||i='O'||i='U')
+	printf("VOWEL");
+	else
+	printf("CONSONANT");
+}
