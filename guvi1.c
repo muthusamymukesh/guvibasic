22ce42115cf5f779f36f1f22d9a49238bbd6145d
shivam2060 <shivam.agrawal_cs15@gla.ac.in>
2018-01-24 21:39:47 +0530

    positive
    1

--- guvi1.c
+++ guvi1.c
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 #include<stdio.h>
 void main()
 {
@@ -11,30 +10,4 @@ void main()
 	printf("NEGATIVE");
 	else
 	printf("ZERO");
-=======
-#include<stdio.h>
-void main()
-{
-	long int i;
-	scanf("%ld",&i);
-	if(i>0)
-	printf("POSITIVE");
-	else if(i<0)
-	printf("NEGATIVE");
-	else
-	printf("ZERO");
->>>>>>> 742dd16a1ab3e5d9a25ffd18d1ac1427a04e1531
-=======
-#include<stdio.h>
-void main()
-{
-	long int i;
-	scanf("%ld",&i);
-	if(i>0)
-	printf("POSITIVE");
-	else if(i<0)
-	printf("NEGATIVE");
-	else
-	printf("ZERO");
->>>>>>> 742dd16a1ab3e5d9a25ffd18d1ac1427a04e1531
 }
--- guvi1.py
+++ guvi1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 x=int(input())
 if x>0:
 	print("positive")
@@ -5,3 +6,12 @@ elif x==0:
 	print("zero")
 else:
 	print("negative")
+=======
+x=int(input())
+if x>0:
+	print("positive")
+elif x==0:
+	print("zero")
+else:
+	print("negative")
+>>>>>>> 742dd16a1ab3e5d9a25ffd18d1ac1427a04e1531
