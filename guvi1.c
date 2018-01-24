1b5cab6873b3829f7d7038c8c91aab69b67ad95e
shivam2060 <shivam.agrawal_cs15@gla.ac.in>
2018-01-24 21:31:20 +0530

    positive

---
+++ guvi1.c
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+#include<stdio.h>
+void main()
+{
+	long int i;
+	scanf("%ld",&i);
+	if(i>0)
+	printf("POSITIVE");
+	else if(i<0)
+	printf("NEGATIVE");
+	else
+	printf("ZERO");
+=======
+#include<stdio.h>
+void main()
+{
+	long int i;
+	scanf("%ld",&i);
+	if(i>0)
+	printf("POSITIVE");
+	else if(i<0)
+	printf("NEGATIVE");
+	else
+	printf("ZERO");
+>>>>>>> 742dd16a1ab3e5d9a25ffd18d1ac1427a04e1531
+}
