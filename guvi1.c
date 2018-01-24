8ab4d9dfca67ab461364ef6151cc248004443cc4
shivam2060 <shivam.agrawal_cs15@gla.ac.in>
2018-01-24 21:51:50 +0530

    Merge branch 'master' of https://github.com/shivam2060/guvibasic

--- guvi1.c
+++ guvi1.c
@@ -10,33 +10,4 @@ void main()
 	printf("NEGATIVE");
 	else
 	printf("ZERO");
-<<<<<<< HEAD
 }
-=======
-=======
-#include<stdio.h>
-void main()
-{
-	int i;
-	scanf("%d",&i);
-	if(i>0)
-	printf("POSITIVE");
-	else if(i<0)
-	printf("NEGATIVE");
-	else
-	printf("ZERO");
->>>>>>> 742dd16a1ab3e5d9a25ffd18d1ac1427a04e1531
-=======
-#include<stdio.h>
-void main()
-{
-	int i;
-	scanf("%d",&i);
-	if(i>0)
-	printf("POSITIVE");
-	else if(i<0)
-	printf("NEGATIVE");
-	else
-	printf("ZERO");
->>>>>>> 
->>>>>>> 3b70744a1ba839020a4cb69e03181df83114a74e
