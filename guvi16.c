50ae5b30b5c8437ed25a1c5bdb5fae834fa88d50
shivam2060 <shivam.agrawal_cs15@gla.ac.in>
2018-01-24 23:58:15 +0530

    prime1

---
+++ guvi16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int i,j,k=0,x;
+	scanf("%d%d",&i,&j);
+	for(b=i+1;b<k:b++)
+	{
+	for(j=2;j<b/2;j++)
+	{
+	if(j%b==0)
+	k=1;
+	}
+	if(k==1)
+	printf("%d",b);
+	}
+}
