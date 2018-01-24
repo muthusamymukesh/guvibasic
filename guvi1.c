3b70744a1ba839020a4cb69e03181df83114a74e
shivam2060 <35771338+shivam2060@users.noreply.github.com>
2018-01-24 21:38:58 +0530

    Update guvi1.c

--- guvi1.c
+++ guvi1.c
@@ -3,8 +3,8 @@
 #include<stdio.h>
 void main()
 {
-	long int i;
-	scanf("%ld",&i);
+	int i;
+	scanf("%d",&i);
 	if(i>0)
 	printf("POSITIVE");
 	else if(i<0)
@@ -15,8 +15,8 @@ void main()
 #include<stdio.h>
 void main()
 {
-	long int i;
-	scanf("%ld",&i);
+	int i;
+	scanf("%d",&i);
 	if(i>0)
 	printf("POSITIVE");
 	else if(i<0)
@@ -28,13 +28,12 @@ void main()
 #include<stdio.h>
 void main()
 {
-	long int i;
-	scanf("%ld",&i);
+	int i;
+	scanf("%d",&i);
 	if(i>0)
 	printf("POSITIVE");
 	else if(i<0)
 	printf("NEGATIVE");
 	else
 	printf("ZERO");
->>>>>>> 742dd16a1ab3e5d9a25ffd18d1ac1427a04e1531
-}
+>>>>>>> 
